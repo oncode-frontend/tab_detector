8fb616b3af213be0dc54d2d32d1a4489a0bf3e5c
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-03-16 15:12:29 +0330

    update text

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const $ = document
 const h1Elem = $.querySelector('h1')
 
 
-document.addEventListener("visibilitychange", (event) => {
+document.addEventListener("visibilitychange", () => {
     if (document.visibilityState == "visible") {
         h1Elem.innerHTML = "you leave me :("
         document.title = "Active"
     } else {
-        h1Elem.innerHTML = "If you leave this page and change tap, I will notice :)"
+        h1Elem.innerHTML = "If you leave this page and change tab, I will notice :)"
         document.title = "Inactive"
     }
   });
